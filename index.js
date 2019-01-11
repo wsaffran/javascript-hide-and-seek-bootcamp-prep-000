b5a8a1f7f27ce5216b931eaa73b8b84153155bd6
William Saffran <willh.saffran@gmail.com>
2019-01-11 17:08:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ function nestedTarget() {
   return document.getElementById('nested').querySelector('.target')
 }
 
-
+function increaseRankBy(n) {
+  const rankedList = document.querySelector('.ranked-list')
+  for (let i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = (parseInt(rankedList[i]) + n)
+  }
+}
 
 
 
