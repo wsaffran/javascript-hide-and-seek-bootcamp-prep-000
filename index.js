c1e8701fc1b802de5b2f705819774d08ac13e6b5
William Saffran <willh.saffran@gmail.com>
2019-01-11 17:26:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  const rankedList = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = (parseInt(rankedList[i]) + n)
+  }
+}
+
+function deepestChild() {
+  const nodeList = document.getElementById('grand-node').querySelectorAll('div')
+  var nextNode
+
+  var i = 0
+  while (i < nodeList.length - 1) {
+    nextNode = nodeList[i].querySelector('div')
+    i++
+  }
+  return nextNode
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target")
+}
+
+function increaseRankBy(n) {
+
+}
+*/
